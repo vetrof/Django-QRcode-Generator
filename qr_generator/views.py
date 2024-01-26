79c6da4d8271e2493639121b078aa8ae62b9b139
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-01-26 14:34:40 +0600

    init2

--- qr_generator/views.py
+++ qr_generator/views.py
@@ -8,7 +8,6 @@ from urllib.parse import urlencode
 
 def generate_qr(request, url):
 
-    # Создаем объект QRCode
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -16,14 +15,11 @@ def generate_qr(request, url):
         border=4,
     )
 
-    # Добавляем URL в QRCode
     qr.add_data(url)
     qr.make(fit=True)
 
-    # Создаем изображение QRCode
     img = qr.make_image(fill_color="black", back_color="white")
 
-    # Возвращаем изображение как ответ HTTP
     response = HttpResponse(content_type="image/png")
     img.save(response, "PNG")
     return response
